bb62fae683c607f3acfcf196168e1aaa65ac144c
Marthen Nodado <projects@mnodado.com>
2024-03-25 23:40:51 -0700

    add api integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.codepath.libraries:asynchttpclient:2.2.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/project5/MainActivity.kt
+++ app/src/main/java/com/example/project5/MainActivity.kt
@@ -2,10 +2,69 @@ package com.example.project5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestHeaders
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.codepath.asynchttpclient.callback.TextHttpResponseHandler
+import okhttp3.Headers
+
+import org.json.JSONArray
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var catNameView: TextView
+    private lateinit var ratingTextView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        catNameView = findViewById(R.id.textView)
+        ratingTextView = findViewById(R.id.textView2)
+
+
+        getCatInfo()
+    }
+
+    private fun getCatInfo() {
+        val privateKey = "THFyIAr2wwXnlHwNUvU+Eg==rppp9Rub6Bi4NFvV"
+        val client = AsyncHttpClient()
+        val headers = RequestHeaders()
+        val params = RequestParams()
+        headers["x-api-key"] = privateKey
+
+//        headers.put("x-api-key", privateKey)
+
+
+        client.get("https://api.api-ninjas.com/v1/cats?name=abyssinian", headers, params, object: JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String?,
+                throwable: Throwable?
+            ) {
+                errorResponse?.let { unwrappedErrorResponse ->
+                    Log.d("cat", "")
+                }
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                json?.let { unwrappedJSON ->
+                    val data = json.jsonArray.getJSONObject(0)
+                    val catName = data["name"]
+                    val rating = data["family_friendly"]
+                    catNameView.text = "$catName"
+                    ratingTextView.text = "Family Friendliness Rating: $rating"
+
+                    Log.d("cat", "response successful$data")
+                    Log.d("cat", "response successful$catName")
+                }
+            }
+        })
     }
 }
